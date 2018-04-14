21ff0e8390c47394ebbc010277bab9d4dc5f54fb
OOP <edgargaytsuk@gmail.com>
2018-04-14 11:33:26 +0300

    DAO+DTO create

---
+++ DAO/src/main/java/com/skillsup/DAO/repo/UserDAO.java
@@ -0,0 +1,8 @@
+package com.skillsup.DAO.repo;
+
+import com.skillsup.DAO.model.User;
+
+public interface UserDAO {
+
+    void create(User user);
+}
---
+++ Services/src/main/java/com/skillsup/services/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.skillsup.services.impl;
+
+import com.skillsup.DAO.repo.UserDAO;
+import com.skillsup.services.DTO.UserDTO;
+import com.skillsup.services.UserServices;
+
+public class UserServiceImpl implements UserServices{
+    private final UserDAO userDao;
+    @Override
+    public void create(UserDTO user) {
+
+    }
+}
