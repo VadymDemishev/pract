a4cab17552d9263c7bc1be1016cafc7c37a39f88
OOP <edgargaytsuk@gmail.com>
2018-04-14 13:15:57 +0300

    Spring configure

--- Main/src/main/java/com/skillsup/main/Main.java
+++ Main/src/main/java/com/skillsup/main/Main.java
@@ -1,7 +1,13 @@
 package com.skillsup.main;
 
+import com.skillsup.DAO.model.User;
+import javafx.application.Application;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-
+        ApplicationContext applicationContext = new GenericXmlApplicationContext("context-main.xml");
+        //applicationContext.getBean(User.class);
     }
 }
--- Services/src/main/java/com/skillsup/services/impl/UserServiceImpl.java
+++ Services/src/main/java/com/skillsup/services/impl/UserServiceImpl.java
@@ -4,10 +4,15 @@ import com.skillsup.DAO.repo.UserDAO;
 import com.skillsup.services.DTO.UserDTO;
 import com.skillsup.services.UserServices;
 import com.skillsup.services.convert.UserConvert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserServices{
     private final UserDAO userDao;
     private final UserConvert userConvert;
+
+    @Autowired
     public UserServiceImpl(UserDAO userDao,UserConvert userConvert) {
         this.userDao = userDao;
         this.userConvert = userConvert;
