6bc7531e948442fa3db438642c793b98a614f8c1
OOP <edgargaytsuk@gmail.com>
2018-04-14 11:49:51 +0300

    create

---
+++ Services/src/main/java/com/skillsup/services/convert/impl/UserConvertImpl.java
@@ -0,0 +1,17 @@
+package com.skillsup.services.convert.impl;
+
+import com.skillsup.DAO.model.User;
+import com.skillsup.services.DTO.UserDTO;
+import com.skillsup.services.convert.UserConvert;
+
+public class UserConvertImpl implements UserConvert{
+    @Override
+    public User toEntity(UserDTO dto) {
+        return new User(dto.getFirstName(),dto.getLastName(),dto.getAge(),null);
+    }
+
+    @Override
+    public UserDTO toDto(User entity) {
+        return null;
+    }
+}
--- Services/src/main/java/com/skillsup/services/impl/UserServiceImpl.java
+++ Services/src/main/java/com/skillsup/services/impl/UserServiceImpl.java
@@ -3,11 +3,19 @@ package com.skillsup.services.impl;
 import com.skillsup.DAO.repo.UserDAO;
 import com.skillsup.services.DTO.UserDTO;
 import com.skillsup.services.UserServices;
+import com.skillsup.services.convert.UserConvert;
 
 public class UserServiceImpl implements UserServices{
     private final UserDAO userDao;
+    private final UserConvert userConvert;
+    public UserServiceImpl(UserDAO userDao,UserConvert userConvert) {
+        this.userDao = userDao;
+        this.userConvert = userConvert;
+    }
+
     @Override
     public void create(UserDTO user) {
 
+        userDao.create(userConvert.toEntity(user));
     }
 }
